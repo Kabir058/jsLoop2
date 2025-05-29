3c82092e8b95cb81e2451f35de3fb09b57c0cd06
Kabir058 <riduanulkabir01@gmail.com>
2025-05-29 21:18:08 +0600

    2 conditions at once

--- continue.js
+++ continue.js
@@ -1,9 +1,18 @@
-for(i=1; i<=40; i++)
-{
-    if(i%2===1)
-    {
-        continue;
+// for(i=1; i<=40; i++)
+// {
+//     if(i%2===1)
+//     {
+//         continue;
 
+//     }
+//     console.log(i)
+// }
+
+for(i=55; i<=85; i++)
+{   
+    if(i%2===0 || i%5===0)
+    {
+        continue
     }
     console.log(i)
 }
