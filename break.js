3c6dfdc17b739f3e099f5a6718764129dfd02cb0
Kabir058 <riduanulkabir01@gmail.com>
2025-05-29 19:27:42 +0600

    for continue

---
+++ break.js
@@ -0,0 +1,22 @@
+// for(i=1; i<=200; i++)
+// {   
+//     if(i>100)
+//     {
+//         break;
+//     }
+// console.log(i)
+// }
+
+// sum=0;
+// for(i=1; i<100; i++)
+    
+// {   sum = sum + i;
+//     if(sum>=100)
+//     {
+//         break
+//     }
+//     console.log(i)
+//     console.log("sum is ",sum)
+// }
+
+for(i=1; i<)
