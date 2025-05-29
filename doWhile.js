cae6dfa3a5c8da0a57605a8f94a83eb6d476467e
Kabir058 <riduanulkabir01@gmail.com>
2025-05-29 10:10:16 +0600

    do while and sum practice

--- doWhile.js
+++ doWhile.js
@@ -1,7 +1,33 @@
-let i=0;
-while(i<=5)
-{
-    console.log(i)
-    i++;
+// let i=0;
+// while(i<=5)
+// {
+//     console.log(i)
+//     i++;
     
+// }
+
+// for(i=61; i<=100; i++)
+// {
+//     if(i%2===1)
+//     {
+//         console.log(i)
+//     }
+// }
+
+// let i =78;
+// while(i<=98)
+// {
+//     if(i%2===0){
+//  console.log(i)
+//     }
+//   i++;     
+// }
+
+
+let n=0;
+do{
+    console.log(n)
+    n++;
 }
+
+while(n<6)
---
+++ sum.js
@@ -0,0 +1,9 @@
+sum=0;
+for(i=81; i<=131; i++)
+{
+    if(i%2===1){
+        console.log(i)
+    sum=sum+i;
+    console.log(sum)}
+}
+console.log("Total sum of odd numbers 81 to 131 is ",sum)
