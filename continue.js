fa3a50c4c434b71b90408c561c91657087352490
Kabir058 <riduanulkabir01@gmail.com>
2025-05-29 21:27:57 +0600

    another part

--- continue.js
+++ continue.js
@@ -8,11 +8,17 @@
 //     console.log(i)
 // }
 
-for(i=55; i<=85; i++)
-{   
-    if(i%2===0 || i%5===0)
-    {
-        continue
+// for(i=55; i<=85; i++)
+// {   
+//     if(i%2===0 || i%5===0)
+//     {
+//         continue
+//     }
+//     console.log(i)
+// }
+
+for (let i = 100; i <= 150; i++) {
+    if (i % 3 === 0 && i % 9 !== 0) {
+        console.log(i);
     }
-    console.log(i)
 }
